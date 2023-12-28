bd26cdae0edd3b7c60b7be79369e65ef3b59852f
TaenAhammed <taenahammed7@gmail.com>
2023-12-29 02:51:34 +0600

    Update test code and cicd workflow

--- Test2/app/app.py
+++ Test2/app/app.py
@@ -1,13 +1,29 @@
-from flask import Flask
-from waitress import serve
-import os
+import http.server
+import socketserver
+import signal
+import sys
 
-app = Flask(__name__)
+PORT = 8000
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'Hello, World!')
+        else:
+            self.send_response(404)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3000))
-    serve(app, host = '0.0.0.0', port = port)
+def run_server():
+    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
+def stop_server(signal, frame):
+    print("Stopping server")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, stop_server)
+    run_server()
--- Test2/app/test_app.py
+++ Test2/app/test_app.py
@@ -1,14 +1,27 @@
 import unittest
-from app import app
+import http.client
+import subprocess
+import time
+import signal
+from app import PORT
 
-class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
+class TestMyHandler(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.server_process = subprocess.Popen(['python3', 'app.py'])
+        time.sleep(1)  # give the server a second to start up
 
-    def test_hello_world(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(), 'Hello, World!')
+    @classmethod
+    def tearDownClass(cls):
+        cls.server_process.send_signal(signal.SIGINT)
+        cls.server_process.wait()
+
+    def test_get_root(self):
+        conn = http.client.HTTPConnection('localhost', PORT)
+        conn.request('GET', '/')
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.read(), b'Hello, World!')
 
 if __name__ == '__main__':
     unittest.main()
